981541216aa1f6eb52cc378d44989b716448edda
PlatovD <platovdima6@gmail.com>
2026-03-27 19:51:08 +0300

    Небольшой рефактор

--- laboratory2.py
+++ laboratory2.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 from typing import Type
 
 import numpy as np
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 
 ANALYTIC_VALUE = -18.11347301218
 
@@ -10,7 +10,7 @@ ANALYTIC_VALUE = -18.11347301218
 class Drawer:
     @staticmethod
     def plot_n_vs_accuracy(
-            integrator_classes: list[Type],
+            integrator_classes: list['NumericalIntegrator'],
             method_names: list[str],
             a: float,
             b: float,
@@ -25,8 +25,7 @@ class Drawer:
             n_values = []
 
             for accuracy in accuracy_values:
-                integrator = integrator_class([a, b], func, 2)
-                _, n_actual = integrator.integrate_with_accuracy(accuracy)
+                _, n_actual = integrator_class.integrate_with_accuracy([a, b], func, accuracy)
                 n_values.append(n_actual)
 
             plt.loglog(accuracy_values, n_values, '-', linewidth=2,
@@ -42,7 +41,7 @@ class Drawer:
 
     @staticmethod
     def plot_errors(
-            integrator_class: Type,
+            integrator_class: 'NumericalIntegrator',
             exact_value: float,
             a: float,
             b: float,
@@ -56,8 +55,7 @@ class Drawer:
         rel_errors = []
 
         for n in n_values:
-            integrator = integrator_class([a, b], func, n)
-            approx = integrator.integrate()
+            approx = integrator_class.integrate([a, b], func, n)
 
             abs_error = abs(approx - exact_value)
             rel_error = abs_error / abs(exact_value) if exact_value != 0 else abs_error
@@ -108,8 +106,7 @@ class Drawer:
             rel_errors = []
 
             for n in n_values:
-                integrator = integrator_class([a, b], func, n)
-                approx = integrator.integrate()
+                approx = integrator_class.integrate([a, b], func, n)
 
                 abs_error = abs(approx - exact_value)
                 rel_error = abs_error / abs(exact_value) if exact_value != 0 else abs_error
@@ -143,176 +140,149 @@ class Drawer:
 
 
 class NumericalIntegrator(ABC):
-    def __init__(self, borders: [], func: callable, intervals_cnt: int):
-        self.borders = borders
-        self.func = func
-        self.intervals_cnt = intervals_cnt
-        self._build_intervals()
+    @staticmethod
+    def _build_intervals(borders: [], intervals_cnt: int):
+        l, r = borders
+        step = (r - l) / intervals_cnt
+        intervals = np.linspace(l + step, r, intervals_cnt)
+        return [(intervals[i], intervals[i + 1])
+                for i in range(intervals.shape[0] - 1)]
 
-    def _build_intervals(self):
-        l, r = self.borders
-        self.intervals = []
-        step = (r - l) / self.intervals_cnt
-        for i in range(self.intervals_cnt):
-            self.intervals.append((l + i * step, l + (i + 1) * step))
-
-    @abstractmethod
-    def integrate(self):
+    @staticmethod
+    def integrate(borders: [], func: callable, intervals_cnt: int):
         pass
 
-    @abstractmethod
-    def build_and_integrate(self, intervals_cnt):
-        pass
-
-    @abstractmethod
-    def integrate_with_accuracy(self, accuracy, max_iterations=1000):
+    @staticmethod
+    def integrate_with_accuracy(borders: [], func: callable, accuracy, max_iterations=100):
         pass
 
 
 class LeftRectangleMethod(NumericalIntegrator):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._cache = dict()
-
-    def integrate_with_accuracy(self, accuracy, max_iterations=10000000):
+    @staticmethod
+    def integrate_with_accuracy(borders: [], func: callable, accuracy, max_iterations=100):
         l, r = 1, max_iterations
         good_iter_cnt = max_iterations // 2
         while l <= r:
             m = l + (r - l) // 2
-            if abs(self.build_and_integrate(m) - self.build_and_integrate(2 * m)) <= accuracy:
+            if abs(LeftRectangleMethod.integrate(borders, f, m) - LeftRectangleMethod.integrate(borders, func,
+                                                                                                2 * m)) <= accuracy:
                 good_iter_cnt = m
                 r = m - 1
             else:
                 l = m + 1
-        return self.build_and_integrate(good_iter_cnt), good_iter_cnt
+        return LeftRectangleMethod.integrate(borders, func, good_iter_cnt), good_iter_cnt
 
-    def integrate(self):
-        return sum(self.func(interval[0]) * (interval[1] - interval[0]) for interval in self.intervals)
-
-    def build_and_integrate(self, intervals_cnt):
-        l, r = self.borders
-        step = (r - l) / intervals_cnt
-        x = np.linspace(l, r - step, intervals_cnt)
-        return np.sum(self.func(x)) * step
+    @staticmethod
+    def integrate(borders: [], func: callable, intervals_cnt: int):
+        intervals = LeftRectangleMethod._build_intervals(borders, intervals_cnt)
+        return sum(func(interval[0]) * (interval[1] - interval[0]) for interval in intervals)
 
 
 class RightRectangleMethod(NumericalIntegrator):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._cache = dict()
-
-    def integrate_with_accuracy(self, accuracy, max_iterations=10000000):
+    @staticmethod
+    def integrate_with_accuracy(borders: [], func: callable, accuracy, max_iterations=100):
         l, r = 1, max_iterations
         good_iter_cnt = max_iterations // 2
         while l <= r:
             m = l + (r - l) // 2
-            if abs(self.build_and_integrate(m) - self.build_and_integrate(2 * m)) <= accuracy:
+            if abs(RightRectangleMethod.integrate(borders, func, m) - RightRectangleMethod.integrate(
+                    borders, func, 2 * m)) <= accuracy:
                 good_iter_cnt = m
                 r = m - 1
             else:
                 l = m + 1
-        return self.build_and_integrate(good_iter_cnt), good_iter_cnt
+        return RightRectangleMethod.integrate(borders, func, good_iter_cnt), good_iter_cnt
 
-    def build_and_integrate(self, intervals_cnt):
-        l, r = self.borders
-        step = (r - l) / intervals_cnt
-        x = np.linspace(l + step, r, intervals_cnt)
-        return np.sum(self.func(x)) * step
-
-    def integrate(self):
-        return sum(self.func(interval[1]) * (interval[1] - interval[0]) for interval in self.intervals)
+    @staticmethod
+    def integrate(borders: [], func: callable, intervals_cnt: int):
+        intervals = RightRectangleMethod._build_intervals(borders, intervals_cnt)
+        return sum(func(interval[1]) * (interval[1] - interval[0]) for interval in intervals)
 
 
 class MiddleRectangleMethod(NumericalIntegrator):
-    def build_and_integrate(self, intervals_cnt):
-        res = 0
-        l, r = self.borders
-        step = (r - l) / intervals_cnt
-        for i in range(intervals_cnt):
-            res += self.func(l + (i + 0.5) * step) * step
-        return res
-
-    def integrate_with_accuracy(self, accuracy, max_iterations=1000):
+    @staticmethod
+    def integrate_with_accuracy(borders: [], func: callable, accuracy, max_iterations=100):
         l, r = 1, max_iterations
         good_iter_cnt = max_iterations // 2
         while l <= r:
             m = l + (r - l) // 2
-            if abs(self.build_and_integrate(m) - self.build_and_integrate(2 * m)) / 3 <= accuracy:
+            if abs(MiddleRectangleMethod.integrate(borders, func, m) - MiddleRectangleMethod.integrate(borders, func,
+                                                                                                       2 * m)) / 3 <= accuracy:
                 good_iter_cnt = m
                 r = m - 1
             else:
                 l = m + 1
-        return self.build_and_integrate(good_iter_cnt), good_iter_cnt
+        return MiddleRectangleMethod.integrate(borders, func, good_iter_cnt), good_iter_cnt
 
-    def integrate(self):
-        return sum(self.func((interval[1] + interval[0]) / 2) * (interval[1] - interval[0]) for interval in
-                   self.intervals)
+    @staticmethod
+    def integrate(borders: [], func: callable, intervals_cnt: int):
+        intervals = MiddleRectangleMethod._build_intervals(borders, intervals_cnt)
+        return sum(func((interval[1] + interval[0]) / 2) * (interval[1] - interval[0]) for interval in
+                   intervals)
 
 
 class TrapezoidMethod(NumericalIntegrator):
-    def integrate(self):
-        return sum((self.func(interval[1]) + self.func(interval[0])) / 2 * (interval[1] - interval[0]) for interval in
-                   self.intervals)
+    @staticmethod
+    def integrate(borders: [], func: callable, intervals_cnt: int):
+        intervals = TrapezoidMethod._build_intervals(borders, intervals_cnt)
+        return sum((func(interval[1]) + func(interval[0])) / 2 * (interval[1] - interval[0]) for interval in
+                   intervals)
 
-    def build_and_integrate(self, intervals_cnt):
-        res = 0
-        l, r = self.borders
-        step = (r - l) / intervals_cnt
-        for i in range(intervals_cnt):
-            res += (self.func(l + (i + 1) * step) + self.func(l + i * step)) / 2 * step
-        return res
-
-    def integrate_with_accuracy(self, accuracy, max_iterations=1000):
+    @staticmethod
+    def integrate_with_accuracy(borders: [], func: callable, accuracy, max_iterations=100):
         l, r = 1, max_iterations
         good_iter_cnt = max_iterations // 2
         while l <= r:
             m = l + (r - l) // 2
-            if abs(self.build_and_integrate(m) - self.build_and_integrate(2 * m)) / 3 <= accuracy:
+            if abs(TrapezoidMethod.integrate(borders, func, m) - TrapezoidMethod.integrate(borders, func,
+                                                                                           2 * m)) / 3 <= accuracy:
                 good_iter_cnt = m
                 r = m - 1
             else:
                 l = m + 1
-        return self.build_and_integrate(good_iter_cnt), good_iter_cnt
+        return TrapezoidMethod.integrate(borders, func, good_iter_cnt), good_iter_cnt
 
 
 class SimpsonMethod(NumericalIntegrator):
-    def integrate(self):
+    @staticmethod
+    def integrate(borders: [], func: callable, intervals_cnt: int):
+        intervals = SimpsonMethod._build_intervals(borders, intervals_cnt)
         return sum(
-            (self.func(interval[0]) + 4 * self.func(interval[1]) + self.func(interval[2])) / 6 * (
-                    interval[2] - interval[0]) for interval in
-            self.intervals)
+            (func(interval[0]) + 4 * func(interval[1]) + func(interval[2])) / 6 * (
+                    interval[2] - interval[0]) for interval in intervals)
 
-    def build_and_integrate(self, intervals_cnt):
-        res = 0
-        if intervals_cnt % 2 != 0:
-            intervals_cnt -= 1
-        l, r = self.borders
-        step = (r - l) / intervals_cnt
-        for i in range(intervals_cnt // 2):
-            res += (self.func(l + i * 2 * step) + 4 * self.func(l + (i * 2 + 1) * step) + self.func(
-                l + (i * 2 + 2) * step)) / 3 * step
-        return res
-
-    def integrate_with_accuracy(self, accuracy, max_iterations=1000):
+    @staticmethod
+    def integrate_with_accuracy(borders: [], func: callable, accuracy, max_iterations=100):
         l, r = 2, max_iterations
         good_iter_cnt = max_iterations // 2
         while l <= r:
             m = l + (r - l) // 2
-            if abs(self.build_and_integrate(m) - self.build_and_integrate(2 * m)) / 15 <= accuracy:
+            if abs(SimpsonMethod.integrate(borders, func, m) - SimpsonMethod.integrate(borders, func,
+                                                                                       2 * m)) / 15 <= accuracy:
                 good_iter_cnt = m
                 r = m - 1
             else:
                 l = m + 1
-        return self.build_and_integrate(good_iter_cnt), good_iter_cnt
+        return SimpsonMethod.integrate(borders, func, good_iter_cnt), good_iter_cnt
 
-    def _build_intervals(self):
-        if self.intervals_cnt % 2 != 0:
-            raise ValueError('В методе Симпсона число интервалов должно быть четно')
-        l, r = self.borders
-        self.intervals = []
-        step = (r - l) / self.intervals_cnt
-        for i in range(self.intervals_cnt // 2):
-            self.intervals.append((l + (i * 2 * step), (l + ((i * 2 + 1) * step)), (l + ((i * 2 + 2) * step))))
+    @staticmethod
+    def _build_intervals(borders: [], intervals_cnt: int):
+        if intervals_cnt % 2 != 0:
+            intervals_cnt -= 1
+        l, r = borders
+        intervals = []
+        step = (r - l) / intervals_cnt
+        for i in range(intervals_cnt // 2):
+            intervals.append((l + (i * 2 * step), (l + ((i * 2 + 1) * step)), (l + ((i * 2 + 2) * step))))
+        return intervals
+
+
+class MonteCarloMethod:
+    def calc_single_integral(self, func: callable, random_points_cnt: int):
+        pass
+
+    def calc_triple_integral(self):
+        pass
 
 
 def f(x: int):
